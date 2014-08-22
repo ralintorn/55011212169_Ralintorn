79ef7b6ca6f16567dfd9c7d19f57b977f659000f
55011212167Per <IMac@MSU>
2014-08-22 14:44:50 +0700

    เพิ่มโค้ด

---
+++ DictionaraCal_swift_w2.playground/section-1.swift
@@ -0,0 +1,15 @@
+class TipCalculator{
+    //1
+    func returnPssibleTip() -> [Int: Double]{
+        let possibleTipInferred = [0.15, 0.18, 0.20]
+        let possibleTipsExlicit:[Double] = [0.15, 0.18, 0.20]
+        var number0fItem = possibleTipInferred.count
+        
+        var retval = [int: Double]()
+        for possbleTip
+            
+    }
+    return retval
+    
+}
+//let tipCalc = TipCalculator(total: 33.25, taxPct: 0.06)
