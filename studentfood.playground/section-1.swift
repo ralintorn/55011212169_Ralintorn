5214813b2446a9bcc0fba51dcc60dfe81a5aaf52
student <ralintorn.gle@gmail.com>
2014-09-03 15:10:30 +0700

    use for

--- studentfood.playground/section-1.swift
+++ studentfood.playground/section-1.swift
@@ -6,12 +6,27 @@ class studentfood{
     init(name:String,food:String){
         self.name = name
         self.food = food
+        
         fooddictionary=[name:food]
     }
     func addSD(name:String,food:String){
         fooddictionary[name] = food
     }
+    func showstudent(allstudent:Dictionary<String,String>){
+        for (key,value) in allstudent{
+            fooddictionary[key] = value
+            println("key:\(key) value:\(value)")
+        }
+        for showName in allstudent.keys{
+            println("name:\(showName)")
+        }
+        for showfood in allstudent.values{
+            println("food:\(showfood)")
+        }
+    }
 }
 let studentfooddata = studentfood(name:"Gle",food:"shabushi")
 studentfooddata.addSD("Ice",food:"pizza")
 studentfooddata.fooddictionary
+studentfooddata.showstudent(["Fai":"swensen"])
+
