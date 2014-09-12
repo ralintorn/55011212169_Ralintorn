b9a469433de5defbaee39c81614bb6ec941148e1
student <ralintorn.gle@gmail.com>
2014-09-12 17:01:16 +0700

    lab

---
+++ Number/Number/ViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  Number
+//
+//  Created by student on 9/12/14.
+//  Copyright (c) 2014 student. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var L1: UILabel!
+    @IBOutlet var L2: UILabel!
+    @IBOutlet var L3: UILabel!
+    
+    @IBAction func b1(sender: AnyObject) {
+        var num = 0
+    }
+    
+    @IBAction func b2(sender: AnyObject) {
+    }
+    
+    @IBAction func b3(sender: AnyObject) {
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
