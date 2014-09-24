f7d046b072b73bece30c7dade2108a92ee5bb3be
student <ralintorn.gle@gmail.com>
2014-09-24 14:40:07 +0700

    Number

--- Number/Number/ViewController.swift
+++ Number/Number/ViewController.swift
@@ -14,15 +14,34 @@ class ViewController: UIViewController {
     @IBOutlet var L2: UILabel!
     @IBOutlet var L3: UILabel!
     
+    var n1 = 0
+    var n2 = 0
+    var n3 = 0
+    
     @IBAction func b1(sender: AnyObject) {
-        var num = 0
+        n1 += 1
+        L1.text = "\(n1)"
     }
     
     @IBAction func b2(sender: AnyObject) {
+        n2 += 1
+        L2.text = "\(n2)"
     }
     
     @IBAction func b3(sender: AnyObject) {
+        n3 += 1
+        L3.text = "\(n3)"
     }
+    @IBAction func reset(sender: AnyObject) {
+        var n1 = 0
+        var n2 = 0
+        var n3 = 0
+        
+        L1.text = "0"
+        L2.text = "0"
+        L3.text = "0"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
